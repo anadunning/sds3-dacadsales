20cb2d957a0c27ae4a281a6466127824e0b527f1
anadunning <anakpedrosa@hotmail.com>
2021-09-12 17:04:56 -0300

    Created Project Dacadsales

---
+++ backend/src/test/java/com/dunningacademy/dacadsales/DacadsalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dunningacademy.dacadsales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DacadsalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
